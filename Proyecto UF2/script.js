5066ad77cd619363553e7ba21c0d1fcadae4c1a4
juanjo-daw <95426160+juanjo-DAW@users.noreply.github.com>
2023-01-31 20:27:32 +0100

    Add files via upload

--- Proyecto UF2/script.js
+++ Proyecto UF2/script.js
@@ -1,34 +1,31 @@
 function guardarHoras(){
     var monday = document.getElementById("lunes");
-    localStorage.setItem("lunes", monday);
+    localStorage.setItem("lunes", monday.value);
 
     var tuesday = document.getElementById("martes");
-    localStorage.setItem("martes", tuesday);
+    localStorage.setItem("martes", tuesday.value);
 
     var wednesday = document.getElementById("miercoles");
-    localStorage.setItem("miercoles", wednesday);
+    localStorage.setItem("miercoles", wednesday.value);
     
     var thursday = document.getElementById("jueves");
-    localStorage.setItem("jueves", thursday);
+    localStorage.setItem("jueves", thursday.value);
     
     var friday = document.getElementById("viernes");
-    localStorage.setItem("viernes", friday);
+    localStorage.setItem("viernes", friday.value);
 
     var saturday = document.getElementById("sabado");
-    localStorage.setItem("sabado", saturday);
+    localStorage.setItem("sabado", saturday.value);
 
     var sunday = document.getElementById("domingo");
-    localStorage.setItem("domingo", sunday);
-
-}
-
-function fechaActual(){
-    const tiempoTranscurrido = Date.now();
-    const hoy = new Date(tiempoTranscurrido);
-    console.log(hoy.toLocaleDateString());
-}
-
-function horasTotalSemana(){
+    localStorage.setItem("domingo", sunday.value);
     
+
+    var total_horas_semana = parseInt(monday.value) + parseInt(tuesday.value) + parseInt(wednesday.value) + parseInt(thursday.value) + parseInt(friday.value) + parseInt(saturday.value) + parseInt(sunday.value);
+    localStorage.setItem("totalHoras",total_horas_semana);
+}
+
+function fechaActual() {
+  return new Date();
 }
 
